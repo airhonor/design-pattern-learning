a9d714c551be3854ef6e8b8be6357f9d24d1871c
honorzhang <honorzhang@tju.edu.cn>
2021-08-31 16:05:49 +0800

    优化摸鱼观察者的输出

--- src/com/hz/design/pattern/observer/AttachFishObserver.java
+++ src/com/hz/design/pattern/observer/AttachFishObserver.java
@@ -1,5 +1,6 @@
 package com.hz.design.pattern.observer;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -9,7 +10,9 @@ import java.util.Date;
  **/
 public class AttachFishObserver {
 
-    public void stopAttachFish(Date date){
-        System.out.println("赶快别看摸鱼了, 这都什么时候了" + date);
+    public void stopAttachFish(Date date) {
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(f.format(date));
+        System.out.println("赶快别看摸鱼了, 看看时间啊，我的哥");
     }
 }
